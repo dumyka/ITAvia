59d9acf0a6286c0b2505166c1100f2a73f2cb56a
dumyka <dumyka1990@gmail.com>
2023-02-10 20:14:07 +0300

    правки

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -20,7 +20,7 @@ public class TestBase {
     Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
     Configuration.browser = System.getProperty("browser", "chrome");
     Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-    Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+    Configuration.remote = System.getProperty("remote");
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability("selenoid:options", Map.<String, Object>of(
